ffee5cb21c71bb38bfeeee1d1319421ae58a9b8b
Ben Yap <contact@benyap.com>
2021-08-21 19:53:18 +1000

    test: add more tests for keyBy

--- src/keyBy/__tests__/keyBy.test.ts
+++ src/keyBy/__tests__/keyBy.test.ts
@@ -8,9 +8,7 @@ describe("keyBy", () => {
       { id: 3, name: "Emily" },
       { id: 4, name: "Jane" },
     ];
-
     const results = keyBy(data, "id");
-
     expect(results).toMatchInlineSnapshot(`
       Object {
         "1": Object {
@@ -27,6 +25,64 @@ describe("keyBy", () => {
         },
         "4": Object {
           "id": 4,
+          "name": "Jane",
+        },
+      }
+    `);
+  });
+
+  test("returns empty for non-existent field", () => {
+    const data = [
+      { id: 1, name: "Bob" },
+      { id: 2, name: "James" },
+      { id: 3, name: "Emily" },
+      { id: 4, name: "Jane" },
+    ];
+    const results = keyBy(data, "nothing" as any);
+    expect(results).toMatchInlineSnapshot(`Object {}`);
+  });
+
+  test("skips empty keys", () => {
+    const data = [
+      { id: 1, name: "Bob", email: "bob@gmail.com" },
+      { id: 2, name: "James", email: "james@gmail.com" },
+      { id: 3, name: "Emily" },
+      { id: 4, name: "Jane" },
+    ];
+    const results = keyBy(data, "email");
+    expect(results).toMatchInlineSnapshot(`
+      Object {
+        "bob@gmail.com": Object {
+          "email": "bob@gmail.com",
+          "id": 1,
+          "name": "Bob",
+        },
+        "james@gmail.com": Object {
+          "email": "james@gmail.com",
+          "id": 2,
+          "name": "James",
+        },
+      }
+    `);
+  });
+
+  test("overwrites duplicate keys", () => {
+    const data = [
+      { id: 1, name: "Bob", email: "bob@gmail.com" },
+      { id: 1, name: "James", email: "james@gmail.com" },
+      { id: 3, name: "Emily" },
+      { id: 3, name: "Jane" },
+    ];
+    const results = keyBy(data, "id");
+    expect(results).toMatchInlineSnapshot(`
+      Object {
+        "1": Object {
+          "email": "james@gmail.com",
+          "id": 1,
+          "name": "James",
+        },
+        "3": Object {
+          "id": 3,
           "name": "Jane",
         },
       }
--- src/keyBy/__tests__/keyByAsMap.test.ts
+++ src/keyBy/__tests__/keyByAsMap.test.ts
@@ -8,9 +8,7 @@ describe("keyByAsMap", () => {
       { id: 3, name: "Emily" },
       { id: 4, name: "Jane" },
     ];
-
     const results = keyByAsMap(data, "id");
-
     expect(results).toMatchInlineSnapshot(`
       Map {
         1 => Object {
@@ -27,6 +25,64 @@ describe("keyByAsMap", () => {
         },
         4 => Object {
           "id": 4,
+          "name": "Jane",
+        },
+      }
+    `);
+  });
+
+  test("returns empty for non-existent field", () => {
+    const data = [
+      { id: 1, name: "Bob" },
+      { id: 2, name: "James" },
+      { id: 3, name: "Emily" },
+      { id: 4, name: "Jane" },
+    ];
+    const results = keyByAsMap(data, "nothing" as any);
+    expect(results).toMatchInlineSnapshot(`Map {}`);
+  });
+
+  test("skips empty keys", () => {
+    const data = [
+      { id: 1, name: "Bob", email: "bob@gmail.com" },
+      { id: 2, name: "James", email: "james@gmail.com" },
+      { id: 3, name: "Emily" },
+      { id: 4, name: "Jane" },
+    ];
+    const results = keyByAsMap(data, "email");
+    expect(results).toMatchInlineSnapshot(`
+      Map {
+        "bob@gmail.com" => Object {
+          "email": "bob@gmail.com",
+          "id": 1,
+          "name": "Bob",
+        },
+        "james@gmail.com" => Object {
+          "email": "james@gmail.com",
+          "id": 2,
+          "name": "James",
+        },
+      }
+    `);
+  });
+
+  test("overwrites duplicate keys", () => {
+    const data = [
+      { id: 1, name: "Bob", email: "bob@gmail.com" },
+      { id: 1, name: "James", email: "james@gmail.com" },
+      { id: 3, name: "Emily" },
+      { id: 3, name: "Jane" },
+    ];
+    const results = keyByAsMap(data, "id");
+    expect(results).toMatchInlineSnapshot(`
+      Map {
+        1 => Object {
+          "email": "james@gmail.com",
+          "id": 1,
+          "name": "James",
+        },
+        3 => Object {
+          "id": 3,
           "name": "Jane",
         },
       }
