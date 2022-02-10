09bd4e35f359259a22d9d994fadfd6736ba39768
Ben Yap <contact@benyap.com>
2022-02-10 11:45:41 +1100

    chore: upgrade dev dependencies

--- src/tryToCatch/tryToCatch.ts
+++ src/tryToCatch/tryToCatch.ts
@@ -1,16 +1,11 @@
-type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
-
-export async function tryToCatch<T extends (...args: any) => any>(
+export async function tryToCatch<T extends (...args: any) => PromiseLike<any>>(
   fn: T,
   ...args: Parameters<T>
-): Promise<[null, Awaited<ReturnType<T>>] | [unknown]> {
+): Promise<readonly [unknown] | readonly [null, Awaited<ReturnType<T>>]> {
   try {
-    const result: ReturnType<T> = Array.isArray(args)
-      ? await fn(...(args as any[]))
-      : await fn(args);
-
-    return [null, result];
+    const result: Awaited<ReturnType<T>> = await fn(...(args as any[]));
+    return [null, result] as const;
   } catch (e: unknown) {
-    return [e];
+    return [e] as const;
   }
 }
